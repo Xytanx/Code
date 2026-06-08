920326b13183a17a4bcb57437dad98308301511a
Xytanx <136854542+Xytanx@users.noreply.github.com>
2026-06-08 15:19:16 +0530

    Time: 5 ms (78.61%) | Memory: 46.2 MB (17.15%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+            return false;
+        int rev=0, n=x;
+        while(n!=0)
+        {
+            int t=n%10;
+            rev=rev*10+t;
+            n/=10;
+        }
+        if(x==rev)
+            return true;
+        return false;
+    }
+}
